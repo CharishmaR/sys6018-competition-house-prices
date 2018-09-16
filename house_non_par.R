bfcf86990bbb256a032d155fd6754281a9989aa8
Beni Shpringer <bshpringer@gmail.com>
2018-09-16 11:56:36 -0400

    move some things around, comment out stuff used for testing

--- house_non_par.R
+++ house_non_par.R
@@ -5,55 +5,7 @@ library(readr)
 library(dplyr) 
 library(caret)
 
-house_train <- read_csv("train.csv")
-house_pred <- read_csv("test.csv")
-
-# After preliminary examination of data set and its descriptions, we first study the relationship between house areas and SalePrice. Specifically, we look at variables LotArea, GrLivArea, GarageArea, OpenPorchSF and PoolArea. 
-
-attach(house_train)
-
-# We observe a moderately strong positive linear relationship between GrLivArea and SalePrice, OpenPorchSF and SalePrice as well as GarageArea and SalePrice. 
-
-
-# filling NA values
-house_pred_nafill <- house_pred
-house_pred_nafill$GarageArea[is.na(house_pred_nafill$GarageArea)] <- mean(house_train$GarageArea)
-
-# Normalizing some Data
-house_train$QualNorm <- OverallQual/mean(OverallQual)
-house_train$GrLivAreaNorm <- GrLivArea/mean(GrLivArea)
-house_train$OpenPorchNorm <- as.numeric(OpenPorchSF > 0)
-#house_train$GarageAreaNorm <- GarageArea/mean(GarageAreaNorm)
-
-
-house_pred$QualNorm <- house_pred$OverallQual/mean(house_pred$OverallQual)
-house_pred$GrLivAreaNorm <- house_pred$GrLivArea/mean(house_pred$GrLivArea)
-
-# CV
-# train <- createDataPartition(house_train$SalePrice, p=0.9, list=FALSE)
-# training <- house_train[train, ]
-# testing <- house_train[-train, ]
-
-# feature enginering
-
-# choosing variables OverallQual, GrLivArea, GarageArea and OpenPorchSF
-# house_train_sub <- select(training,OverallQual,GrLivArea,SalePrice,Neighborhood)
-# house_pred_sub <- select(testing,OverallQual,GrLivArea, SalePrice,Neighborhood)
-# reg_output <- data.frame(house_pred_sub, knn(house_train_sub, house_pred_sub))
-
-
-# testing the normal method vs with normalized data
-norm_train_sub <- select(house_train,QualNorm,GrLivAreaNorm,Neighborhood,SalePrice)
-norm_test_sub <- select(house_pred,QualNorm,GrLivAreaNorm,Neighborhood)
-#norm_output <- data.frame(norm_test_sub, knn(norm_train_sub, norm_test_sub))
-#reg_output$reg_output_resid = abs(reg_output$SalePrice - reg_output$knn.house_train_sub..k_output..house_pred_sub.)
-#norm_output$norm_output_resid = abs(norm_output$SalePrice - norm_output$knn.norm_train_sub..k_output..norm_test_sub.)
-
-#reg_ss = mean(reg_output$reg_output_resid)
-#norm_ss = mean(norm_output$norm_output_resid)
-
-#norm_ss/reg_ss
-
+# Define key functions
 # Euclidean distance
 edist <- function(a, b){
   d = 0
@@ -88,5 +40,66 @@ knn <- function(trainer, predictions){
   return(price_all)
 }
 
+# Read in data
+house_train <- read_csv("train.csv")
+house_pred <- read_csv("test.csv")
+
+# After preliminary examination of data set and its descriptions, we first study the relationship between house areas and SalePrice. Specifically, we look at variables LotArea, GrLivArea, GarageArea, OpenPorchSF and PoolArea. 
+
+attach(house_train)
+
+# We observe a moderately strong positive linear relationship between GrLivArea and SalePrice, OpenPorchSF and SalePrice as well as GarageArea and SalePrice. 
+
+# filling NA values
+house_pred_nafill <- house_pred
+house_pred_nafill$GarageArea[is.na(house_pred_nafill$GarageArea)] <- mean(house_train$GarageArea)
+
+# Normalizing some Data
+house_train$QualNorm <- (OverallQual - mean(OverallQual))/sd(OverallQual)
+house_train$GrLivAreaNorm <- (GrLivArea - mean(GrLivArea))/sd(GrLivArea)
+# house_train$OpenPorchNorm <- as.numeric(OpenPorchSF > 0)
+# house_train$PoolNorm <- as.numeric(PoolArea > 0)
+# house_train$GarageNorm <- GarageArea/mean(GarageArea)
+# house_train$BsmtNorm <- TotalBsmtSF/mean(TotalBsmtSF)
+# house_train$new_old <- as.numeric(YearBuilt > 1995)
+# house_train$LotAreaNorm <- LotArea/mean(LotArea)
+
+house_pred$QualNorm <- (house_pred$OverallQual - mean(house_pred$OverallQual))/sd(house_pred$OverallQual)
+house_pred$GrLivAreaNorm <- (house_pred$GrLivArea - mean(house_pred$GrLivArea))/sd(house_pred$GrLivArea)
+# house_pred$GarageNorm <- house_pred$GarageArea/mean(house_pred$GarageArea)
+# house_pred$BsmtNorm <- house_pred$TotalBsmtSF/mean(house_pred$TotalBsmtSF)
+# house_pred$BsmtNorm <- house_pred$LotArea/mean(house_pred$LotArea)
+# house_pred$new_old <-  as.numeric(house_pred$YearBuilt > 1995)
+
+
+
+# CV
+set.seed(3)
+train <- createDataPartition(house_train$SalePrice, p=0.7, list=FALSE)
+training <- house_train[train, ]
+testing <- house_train[-train, ]
+
+# feature enginering
+
+# choosing variables OverallQual, GrLivArea, GarageArea and OpenPorchSF
+# house_train_sub <- select(training,OverallQual, GrLivArea, GarageArea,OpenPorchSF, SalePrice,Neighborhood)
+# house_pred_sub <- select(testing, OverallQual, GrLivArea, GarageArea, OpenPorchSF, SalePrice,Neighborhood)
+# reg_output <- data.frame(house_pred_sub, knn(house_train_sub, house_pred_sub))
+
+
+# testing with normalized data
+norm_train_sub <- select(training, QualNorm, GrLivAreaNorm, Neighborhood, SalePrice)
+norm_test_sub <- select(testing, QualNorm, GrLivAreaNorm, Neighborhood)
+norm_output <- data.frame(testing, knn(norm_train_sub, norm_test_sub))
+
+# Below are some things for testing out the model with CV
+# norm_train_sub <- select(training, QualNorm, GrLivAreaNorm, OpenPorchNorm, Neighborhood, SalePrice)
+# norm_test_sub <- select(testing, QualNorm, GrLivAreaNorm, OpenPorchNorm, Neighborhood)
+# norm_output <- data.frame(norm_test_sub, knn(norm_train_sub, norm_test_sub))
+# reg_output$reg_output_resid = abs(reg_output$SalePrice - reg_output$knn.house_train_sub..k_output..house_pred_sub.)
+# norm_output$norm_output_resid = abs(norm_output$knn.norm_train_sub..norm_test_sub. - testing$SalePrice)
+# mean(norm_output$norm_output_resid)
+# plot(norm_output$SalePrice, norm_output$norm_output_resid)
+
 house_output <- data.frame(house_pred$Id, knn(norm_train_sub, norm_test_sub))
-write.table(house_output, file = "house_nonpar_after.csv", row.names=F, col.names=c("Id", "SalePrice"), sep=",")
+write.table(house_output, file = "house_nonpar.csv", row.names=F, col.names=c("Id", "SalePrice"), sep=",")
